1fb6e62051670997cc5b672783862d989e27ab9e
nyaran <Nyaran2910@gmail.com>
2026-06-03 20:18:18 +0900

    Remove iPhone project after repo split

--- iPhone/Knode/App/AppEnvironment.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum AppEnvironment {
-    static let fallbackBaseURL = URL(string: "https://obsidian-quiz-maker.vercel.app")!
-    static let localDevelopmentBaseURL = URL(string: "http://localhost:3000")!
-}
